a34f48b899edf385a608bd551a166711b8c7321b
Mazhar <syedmazharhussainshah7@gmail.com>
2026-09-21 15:00:55 +0500

    Slow donations banner marquee to 20s

--- tailwind.config.js
+++ tailwind.config.js
@@ -71,7 +71,7 @@ module.exports = {
         "scale-in": "scale-in 0.15s ease-out both",
         shimmer: "shimmer 1.6s infinite linear",
         float: "float 5s ease-in-out infinite",
-        marquee: "marquee 5s linear infinite",
+        marquee: "marquee 20s linear infinite",
       },
     },
   },
